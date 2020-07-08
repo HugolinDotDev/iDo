2d7da25c47094bafc279194d42c2515d7aac9382
FoxGlow <hugolinma@gmail.com>
2020-07-09 01:45:40 +0200

    Add Date_valid and is_leap functions

--- include/date.h
+++ include/date.h
@@ -6,17 +6,25 @@
 #include <stdbool.h>
 
 #include "../include/fatal.h"
+#include "../include/data_check.h"
 
 #ifndef DATE_SIZE
     #define DATE_SIZE 11
 #endif
+#ifndef MIN_YEAR
+    #define MIN_YEAR 1800
+#endif
+#ifndef MAX_YEAR
+    #define MAX_YEAR 9999
+#endif
 
 typedef struct Date {
-    unsigned int day;
-    unsigned int month;
-    unsigned int year;
+    int day;
+    int month;
+    int year;
     char toString[DATE_SIZE];
 } Date;
 
 extern Date* Date_factory(const char* date_str);
+extern bool Date_valid(Date* date);
 extern void Date_destructor(Date* date);
--- src/date.c
+++ src/date.c
@@ -5,17 +5,60 @@ Date* Date_factory(const char* date_str)
     Date* date = malloc(sizeof(Date));
     fatal(date != NULL, "date is a null pointer", 1);
     strcpy(date->toString, date_str);
-
     char string[DATE_SIZE];
     strcpy(string, date_str);
-    char* tmp = strtok(string, "-");
-    date->day = atoi(tmp);
-    tmp = strtok(NULL, "-");
-    date->month = atoi(tmp);
-    tmp = strtok(NULL, "-");
-    date->year = atoi(tmp);
-
+    char* day = NULL;
+    char* month = NULL;
+    char* year = NULL;
+    day = strtok(string, "-");
+    month = strtok(NULL, "-");
+    year = strtok(NULL, "-");
+    if (!is_number(day) && !is_number(month) && !is_number(year))
+    {
+        Date_destructor(date);
+        return NULL;
+    }
+    date->day = atoi(day);
+    date->month = atoi(month);
+    date->year = atoi(year);
     return date;
+}
+
+bool is_leap(int year)
+{
+    return (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)); 
+}
+
+bool Date_valid(Date* date)
+{
+    if (date->year < MIN_YEAR || date->year > MAX_YEAR)
+        return false;
+    if (date->month < 1 || date->month > 12)
+        return false;
+    if (date->day < 1 || date->day > 31)
+        return false;
+    if (date->month == 2)
+    {
+        if (is_leap(date->year))
+        {
+            if (date->day <= 29)
+                return true;
+            return false;
+        }
+        else
+        {
+            if (date->day <= 28)
+                return true;
+            return false;
+        }
+    }
+    if (date->month == 4 || date->month == 6 || date->month == 9 || date->month == 11)
+    {
+        if (date->day <= 30)
+            return true;
+        return false;
+    }
+    return true;
 }
 
 void Date_destructor(Date* date)
