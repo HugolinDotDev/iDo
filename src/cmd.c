7d21872a0b7eff0920d31fe3e9a67544a487972f
FoxGlow <hugolinma@gmail.com>
2020-07-09 21:32:22 +0200

    Add full edit command and add shortcuts

--- src/cmd.c
+++ src/cmd.c
@@ -2,15 +2,18 @@
 
 void print_help()
 {
-    printf(YEL "Usage : " RST "todoit COMMAND [OPTIONS]\n\n"
-            GRN "la          " RST "Print the tasks with details\n"
-            GRN "ls          " RST "Print the tasks\n"
-            GRN "add         " RST "Add a task\n"
-            GRN "edit        " RST "Edit a task\n"
-            GRN "rm          " RST "Delete a task\n"
-            GRN "tick        " RST "Complete a task\n"
-            GRN "ntick       " RST "Uncomplete a task\n"
-            GRN "find        " RST "Find a task by its id\n");
+    printf(YEL "Usage : " RST "ido COMMAND <...>\n\n"
+                "<id>                             " GRN "Shortcut for find\n"
+            RST "find <id>                        " GRN "Find a task with it's id\n"
+            RST "la                               " GRN "Print the tasks with details\n"
+            RST "ls                               " GRN "Print the tasks\n"
+            RST "rm <id>                          " GRN "Delete a task with it's id\n"
+            RST "tick <id>                        " GRN "Complete a task with it's id\n"
+            RST "ntick <id>                       " GRN "Uncomplete a task with it's id\n"
+            RST "edit/e <id> <" CYN "flag" RST "> <value>       " GRN "Edit a task, editable flags are below :\n"
+            CYN "  -text/-t     " GRN "                    => task's text (max 200 characters, must not include ';')\n"
+            CYN "  -priority/-p " GRN "                    => task's priority\n"
+            CYN "  -end/e       " GRN "                    => task's deadline (date)\n");
 }
 
 bool check_cmd_exists(const char* cmd, const char* cmds[], unsigned int len)
@@ -28,7 +31,7 @@ bool process_cmd(int argc, char* argv[])
 {
     char cmd[10];
     strcpy(cmd, argv[1]);
-    if (!strcmp(cmd, "h"))
+    if (!strcmp(cmd, "h") || !strcmp(cmd, "help"))
     {
         print_help();
     }
@@ -54,11 +57,11 @@ bool process_cmd(int argc, char* argv[])
         }
         Tasks_destructor(tasks);
     }
-    else if (!strcmp(cmd, "tick") || !strcmp(cmd, "ntick"))
+    else if (!strcmp(cmd, "t") || !strcmp(cmd, "tick") || !strcmp(cmd, "nt") || !strcmp(cmd, "ntick"))
     {
         if (argc < 3)
         {
-            printf(RED "Argument 'id' missing " RST "-> todoit (n)tick <id>\n");
+            printf(RED "Argument 'id' missing " RST "-> todoit (n)tick/(n)t <id>\n");
             return false;
         }
         if (!is_number(argv[2]))
@@ -78,7 +81,7 @@ bool process_cmd(int argc, char* argv[])
         {
             if (tasks->arr[i]->id == atoi(argv[2]))
             {
-                if (!strcmp(cmd, "tick") && !tasks->arr[i]->accomplished)
+                if ((!strcmp(cmd, "t") || !strcmp(cmd, "tick")) && !tasks->arr[i]->accomplished)
                 {
                     Task_set_state(tasks->arr[i], true);
                     rewrite_tasks(FILENAME, tasks);
@@ -87,11 +90,11 @@ bool process_cmd(int argc, char* argv[])
                     Tasks_destructor(tasks);
                     return true;
                 }
-                else if (!strcmp(cmd, "tick") && tasks->arr[i]->accomplished)
+                else if ((!strcmp(cmd, "t") || !strcmp(cmd, "tick")) && tasks->arr[i]->accomplished)
                 {
                     printf(RED "Task already completed\n" RST);
                 }
-                else if (!strcmp(cmd, "ntick") && tasks->arr[i]->accomplished)
+                else if ((!strcmp(cmd, "nt") || !strcmp(cmd, "ntick")) && tasks->arr[i]->accomplished)
                 {
                     Task_set_state(tasks->arr[i], false);
                     rewrite_tasks(FILENAME, tasks);
@@ -113,11 +116,11 @@ bool process_cmd(int argc, char* argv[])
         Tasks_destructor(tasks);
         return false;
     }
-    else if (!strcmp(cmd, "find"))
+    else if (!strcmp(cmd, "f") || !strcmp(cmd, "find"))
     {
         if (argc < 3)
         {
-            printf(RED "Argument 'id' missing " RST "-> todoit find <id>\n");
+            printf(RED "Argument 'id' missing " RST "-> todoit find/f <id>\n");
             return false;
         }
         if (!is_number(argv[2]))
@@ -135,11 +138,11 @@ bool process_cmd(int argc, char* argv[])
         Task_pretty(task);
         Task_destructor(task);
     }
-    else if (!strcmp(cmd, "edit"))
+    else if (!strcmp(cmd, "e") || !strcmp(cmd, "edit"))
     {
         if (argc < 5)
         {
-            printf(RED "Some arguments are missing " RST "-> todoit edit <id> <field> <value>\n");
+            printf(RED "Some arguments are missing " RST "-> todoit edit/e <id> <flag> <value>\n");
             return false;
         }
         if (!is_number(argv[2]))
@@ -147,8 +150,9 @@ bool process_cmd(int argc, char* argv[])
             printf(RED "Argument 'id' must be a null or positive number, '%s' given\n" RST, argv[2]);
             return false;
         }
-        const char* FIELDS[] = { "text", "t", "end", "e", "priority", "p" };
-        check_cmd_exists(argv[3], FIELDS, 6);
+        const char* FIELDS[] = { "-text", "-t", "-end", "-e", "-priority", "-p" };
+        if (!check_cmd_exists(argv[3], FIELDS, 6))
+            return false;
         Tasks* tasks = NULL;
         read_tasks(FILENAME, &tasks);
         if (tasks->count == 0)
@@ -157,7 +161,7 @@ bool process_cmd(int argc, char* argv[])
             Tasks_destructor(tasks);
             return true;
         }
-        if (!strcmp(argv[3], "p") || !strcmp(argv[3], "priority"))
+        if (!strcmp(argv[3], "-p") || !strcmp(argv[3], "-priority"))
         {
             if (!check_priority(argv[4]))
             {
@@ -176,7 +180,7 @@ bool process_cmd(int argc, char* argv[])
                 }
             }
         }
-        else if (!strcmp(argv[3], "e") || !strcmp(argv[3], "end"))
+        else if (!strcmp(argv[3], "-e") || !strcmp(argv[3], "-end"))
         {
             if (!check_date(argv[4]))
             {
@@ -198,7 +202,22 @@ bool process_cmd(int argc, char* argv[])
         }
         else
         {
-            
+            if (!check_text(argc, argv))
+            {
+                printf(RED "Argument 'value' must be a valid string\n" RST);
+                Tasks_destructor(tasks);
+                return false;
+            }
+            for (unsigned int i = 0; i < tasks->count; i++)
+            {
+                if (tasks->arr[i]->id == atoi(argv[2]))
+                {
+                    create_text(argc, argv, tasks->arr[i]->text);
+                    rewrite_tasks(FILENAME, tasks);
+                    printf(GRN "Task #%d text edited\n" RST, tasks->arr[i]->id);
+                    Task_pretty(tasks->arr[i]);
+                }
+            }
         }
         Tasks_destructor(tasks);
     }
@@ -245,11 +264,26 @@ bool process_cmd(int argc, char* argv[])
 
 int parse_cmd(int argc, char* argv[])
 {
-    const char* COMMANDS[] = { "h", "la", "ls", "add", "edit", "rm", "tick", "ntick", "find", "init" };
+    const char* COMMANDS[] = { "h", "help", "la", "ls", "add", "a", "edit", "e", "rm", "tick", "t", "ntick", "nt", "find", "f", "init" };
     int status = 0;
     if (argc > 1)
     {
-        if (check_cmd_exists(argv[1], COMMANDS, 10))
+        if (is_number(argv[1]))
+        {
+            Task* task = NULL;
+            read_task(FILENAME, atoi(argv[1]), &task);
+            if (task == NULL)
+            {
+                printf(RED "Task with id #%s not found\n" RST, argv[1]);
+                status = 1;
+            }
+            else
+            {
+                Task_pretty(task);
+                Task_destructor(task);
+            }
+        }
+        else if (check_cmd_exists(argv[1], COMMANDS, 16))
         {
             if (!process_cmd(argc, argv)) status = 1;
         }
@@ -260,7 +294,8 @@ int parse_cmd(int argc, char* argv[])
     }
     else
     {
-        printf(YEL "Type 'h' for help\n" RST);
+        printf(CYN "iDo, easy todo list,\nwritten in C by Hugolin MARIETTE.\n\n" RST
+               YEL "Type 'h' for help\n" RST);
     }
     return status;
 }
