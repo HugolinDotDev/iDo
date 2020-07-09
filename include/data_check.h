fd8fa6a52ed9803cae9aa6ee3a06caf3a9118bd2
FoxGlow <hugolinma@gmail.com>
2020-07-09 21:34:10 +0200

    Add check_text, create_text and is_valid_text functions

--- include/data_check.h
+++ include/data_check.h
@@ -2,6 +2,13 @@
 
 #include "../include/date.h"
 
+#ifndef MAX_TEXT
+    #define MAX_TEXT 201
+#endif
+
 extern bool is_number(const char* string);
+extern bool is_valid_text(const char* text);
 extern bool check_priority(const char* priority);
 extern bool check_date(const char* date);
+extern void create_text(int argc, char* argv[], char* buffer);
+extern bool check_text(int argc, char* argv[]);
--- src/data_check.c
+++ src/data_check.c
@@ -10,6 +10,16 @@ bool is_number(const char* string)
     return true;
 }
 
+bool is_valid_text(const char* text)
+{
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if (text[i] < 32 || text[i] == 59 || text[i] == 127)
+            return false;
+    }
+    return true;
+}
+
 bool check_priority(const char* priority)
 {
     if (strlen(priority) == 1 && priority[0] >= 40 && priority[0] <= 90)
@@ -31,4 +41,31 @@ bool check_date(const char* date_str)
         }
     }
     return false;
+}
+
+void create_text(int argc, char* argv[], char* buffer)
+{
+    buffer[0] = '\0';
+    for (int i = 4; i < argc; i++)
+    {
+        if (i != (argc - 1))
+        {
+            strcat(buffer, argv[i]);
+            strcat(buffer, " ");
+        }
+        else
+        {
+            strcat(buffer, argv[i]);
+            strcat(buffer, "\0");
+        }
+    }
+}
+
+bool check_text(int argc, char* argv[])
+{
+    char buffer[MAX_TEXT] = "";
+    create_text(argc, argv, buffer);
+    if (!is_valid_text(buffer))
+        return false;
+    return true;
 }
